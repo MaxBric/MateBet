9dd95127fba3e0f6e9a0aa950d5829721f8a8c17
Unknown <lhommegwennael@gmail.com>
2018-03-22 15:29:25 +0700

    Creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { BetComponent } from './components/bet/bet.component';
+import { TeamAComponent } from './components/team-a/team-a.component';
+import { TeamBComponent } from './components/team-b/team-b.component';
+import { TeamListComponent } from './components/team-list/team-list.component';
+import { MatchComponent } from './components/match/match.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    BetComponent,
+    TeamAComponent,
+    TeamBComponent,
+    TeamListComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule
